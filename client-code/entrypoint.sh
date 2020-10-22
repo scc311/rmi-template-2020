9ca4d5a54eecb93f606d24fa5f158a4b4261e295
rrryyaaannnnn <ryan.tb.warren@gmail.com>
2020-10-22 19:01:06 +0100

    Added ability to compile packaged classes to 'target' folder and set classpath of rmiregistry

--- client-code/entrypoint.sh
+++ client-code/entrypoint.sh
@@ -6,4 +6,4 @@ sleep 1
 echo "Running Your Client..."
 
 # change me to java <file name with main in it (without .java)>
-java calculatorclient
+java -cp target/ calculatorclient
--- server-code/entrypoint.sh
+++ server-code/entrypoint.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 echo "Running JAVA RMI Registry..."
-rmiregistry &
+rmiregistry -J-Djava.class.path="target/" &
 sleep 1
 
 echo "Running Your Server..."
 
 # change me to java <file name with main in it (without .java)>
-java calculatorserver
+java -cp target/ calculatorserver
